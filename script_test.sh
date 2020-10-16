adebcea1fc25da037d3f5ae95af2511cffb158c9
reda-guendouz <reda.guendouz777@gmail.com>
2020-10-16 11:02:44 +0200

    test

--- script_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./calculatrice_rpn.sh 2 2 +
-./calculatrice_rpn.sh 5 8 -
-#./calculatrice_rpn.sh 9 11 x
-#./calculatrice_rpn.sh 54 12 /
-#./calculatrice_rpn.sh 25 36 + 47 -
-./calculatrice_polonaise_inverse.sh 15 26 71 + -
-./calculatrice_rpn.sh 5 6 7 + +
